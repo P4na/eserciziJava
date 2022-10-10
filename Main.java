76d43c4bd4410fd6809003e075799ec61d472027
P4na <Vip_P4na@protonmail.com>
2022-10-10 21:04:11 +0200

    Risolto qualche esercizio, nulla di troppo interessante

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args)
+    {
+//        es lancia dado
+/*      int numeroFacce = 6;
+        Dado dado = new Dado();
+        System.out.println(dado.lanciaDado(numeroFacce));*/
+
+
+ /*       ArrayList<String> nomi = new ArrayList<>(){
+            {
+                add("marco");
+                add("gino");
+                add("lisa");
+            }
+        };
+        System.out.println(EstraiStringhe.estrai(nomi));*/
+
+    }
+
+
+}
